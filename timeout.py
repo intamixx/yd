5ffea480fbd50f5a21cb866c9827943a230889a4
Intamixx <intamixx@users.noreply.github.com>
2023-09-07 15:03:19 +0100

    Update timeout.py

--- timeout.py
+++ timeout.py
@@ -28,10 +28,35 @@ class Command(object):
         #print (self.process.returncode)
         return (self.process.returncode, self.out)
 
-command = Command("ls -al;uname -a;hostname;sleep 3")
+#command = Command("ls -al;uname -a;hostname;sleep 3")
 #command = Command("echo 'Process started'; sleep 10; echo 'Process finished'")
+
+container = "mpioperator/mpi-pi"
+container_args = "/home/mpiuser/pi"
+
+cmd_str = "docker pull {}".format(container)
+print ("Launching: {}".format(cmd_str))
+command = Command(cmd_str)
 threads = []
 output = command.run(timeout=5)
-print (output[0])
-print (output[1])
+print ("Return code: {}".format(output[0]))
+print ("Output:\n {}".format(output[1]))
+#print (output[0])
+#print (output[1])
+
+cmd_str = "docker run --rm -v /mnt:/mnt {} {}".format(container, container_args)
+print ("Launching: {}".format(cmd_str))
+command = Command(cmd_str)
+threads = []
+output = command.run(timeout=5)
+print ("Return code: {}".format(output[0]))
+print ("Output:\n {}".format(output[1]))
+
+cmd_str = "ls -la"
+print ("Launching: {}".format(cmd_str))
+command = Command(cmd_str)
+threads = []
+output = command.run(timeout=5)
+print ("Return code: {}".format(output[0]))
+print ("Output:\n {}".format(output[1]))
 sys.exit(0)
